364c8e0cc4ffc5e9307c14ac15554f5db9eff9e4
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-06-14 11:46:36 +0200

    Added bounce

--- Breakout/Assets/Scripts/Ball.cs
+++ Breakout/Assets/Scripts/Ball.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+    private Rigidbody2D rb2d;
+    public float forceMag = 1f;
+
 	// Use this for initialization
 	void Start () {
-	
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(new Vector2(forceMag, forceMag));
 	}
 	
 	// Update is called once per frame
---
+++ Breakout/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelHandler : MonoBehaviour
+{
+    private bool isStarted = false;
+    private int amountOfBlocks;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame() { }
+    public void EndGame() { }
+}
