c62f055dfef30da5d45b0792887305539f33d25f
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-07-16 00:00:25 +0200

    Credits screen.

---
+++ Breakout/Assets/Scripts/ScrollingText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingText : MonoBehaviour {
+
+    public float speed = 0.1f;
+    private float x, y, z;
+
+	// Use this for initialization
+	void Start () {
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        y += speed * Time.deltaTime;
+        transform.position = new Vector3(x, y, z);
+	}
+}
