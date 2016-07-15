136391cf423d5b96378c19ffdcad676fc686f426
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-07-15 16:44:35 +0200

    Scrolling menu background situation.

---
+++ Breakout/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public Material mat;
+    public Vector2 scrollSpeed;
+    private Vector2 offset; 
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mat.mainTextureOffset += scrollSpeed*Time.deltaTime;
+
+       // mat.SetTextureOffset(texID,offset + scrollSpeed * Time.deltaTime);
+	}
+}
---
+++ Breakout/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    // public Button playButton, optionsButton, creditsButton, quitButton;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Back"))
+            Quit();
+    }
+
+    public void SetScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
